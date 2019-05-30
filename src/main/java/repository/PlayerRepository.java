9c938a4c0ff502b3573f8138cfdf93a5d38e20b2
mani <mani@example.com>
2019-05-30 20:46:00 +0300

    bob

--- src/main/java/repository/PlayerRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import entity.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player,Integer> {
-
-}
--- src/main/java/repository/TeamRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import entity.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team,Integer> {
-}
